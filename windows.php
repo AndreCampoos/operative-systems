d6a62c50101a9be5a9ab1941dd6b0aa8b781cbfb
André Campos <andrecampos911@gmail.com>
2024-01-22 00:33:01 +0000

    add windows evolution

--- windows.php
+++ windows.php
@@ -61,6 +61,46 @@
     O Windows é um sistema operativo desenolvido pela Microsoft e, atualmente um dos mais utilizados em todo o mundo.
     <br>
     Ao longo dos anos, a Microsoft lançou várias versões deste sistema operativo, fazendo sempre melhorias e colocando novos recursos. As versões do windows mais conhecidas são o Windows 95, o Windows XP, o Windows 7, 8, 10 e 11.
+    <br>
+      <br>
+    O objetivo deste era facilitar a utilização do computador utilizando a interface gráfica (GUI) e o mouse, e assim, ao invés do utilizaor memorizar e escrever comandos complexos, basta selecionar as opções na tela.
+    <br>
+      <br>
+    <b>Windows 1.0</b>
+    <br>
+    Lançado a 20 de novembro de 1985, o windows 1.0 foi o primeiro lançamento, podendo então dizer que foi onde tudo começou. Este não era bem um sistema operativo e sim uma interface gráfica, sendo o seu principal elemento o gestor de ficheiros.
+    <br>
+    Esta primeira versão do windows esteve longe de ser um sucesso, pois foi apenas uma das várias iinterfaces gráficas que nasceram após o lançamento do Macintosh. Uma das prinicpais criticas que este sofria era relativamente ao peso do sistema, que exigia no minimo, um PC XT.
+    <br>
+      <br>
+    <b>Windows 2.0</b>
+    <br>
+    Lançado em 1987, esta foi a primeira versão do windows onde uma aplicação podia-se sobrepor a outra, e foi onde nasceu também as opção de maximizar e minimizar.
+    <br>
+    O windows 2.0 também foi a primira versão capaz de aproveitar o poder de processamento dos Intel 286 e 386, onde era possível executar múltiplos programas ao mesmo tempo, cada um numa sessão do MS-DOS.
+    <br>
+      <br>
+    <b>Windows 3.x</b>
+    <br>
+    O windows 3.x foi a primeira versão a ter um sucesso considerável, recebendo vários elogios relativamente à sua capacidade de mmultitarefa e facilidade de utilização. Este também trouxe inovações, sendo uma delas o gestor de programas.
+    <br>
+    Este foi lançado no ano de 1990.
+    <br>
+      <br>
+    <b>Windows 95</b>
+    <br>
+    O windows 95 cresceu muito rápido, pois o seu lancamento foi apoiado por uma campanha de marketing bilionária, que utilizou uma musica dos Rolling Stones, intitulada de "Start me Up".
+    <br>
+    Neste sistema, foi estreado o menu iniciar e a barra de tarefas e, o seu objetivo era facilitar o upgrade do computador attravés da tecnologia Plug & Play. Para isto, era necessário conectar uma placa de expanção e instalar um driver para começar a utilizá-lo, sem o utilizaodr ter de se preocupar com configurações e endereços de memórias.
+    <br>
+      <br>
+    <b>Windows 98</b>
+    Lançado em 1998, este tinha como foco principal a Internet. O Internet Explorer tornou-se um componente deste sistema operativo e a este também foi conectado um recurso intitulado de "Active Desktop", que permitia visualizar informações vindas de websites e atualizadas sempre que o computador é conectado à internet. Este também foi o pioneiro, no que toca ao suporte a USB's, drives de DVD e ao baramento AGP.
+    <br>
+      <br>
+    <b>Windows Me</b>
+    <br>
+    O Windows Millenium Edition foi um intermediário entre o windows 98 e o windows XP que ficou apenas 1 ano no mercado. Esta foi a última versão que rodava no MS-DOS e tinha como objetivo melhorar o suporte da criação de conteuído, com a ajuda de várias aplicações como o Windows Movie Maker, o Movie Maker, o Windows DVD Player e o Windows Media Player.
   </div>
 </body>
 </html>
